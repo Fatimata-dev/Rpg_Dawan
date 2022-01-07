9cb314b2ad829e4a2319023db4777e2043f92629
Fatimata-dev <dramefatim75@gmail.com>
2022-01-07 09:32:52 +0100

    Initial commit

---
+++ src/Personnage.java
@@ -0,0 +1,11 @@
+public class Personnage {
+    private String nom;
+    private double xp;
+    private int hp;
+    private  int force;
+    private int defense;
+    private double taille;
+    private double poids;
+    private String origine;
+    private double mana;
+}
---
+++ src/Race.java
@@ -0,0 +1,5 @@
+public class Race {
+    private String nom;
+    private  int attaqueSpecial;
+
+}
